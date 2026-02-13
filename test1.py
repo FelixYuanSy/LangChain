372f9958472bc5336f8bb4c9d76deeafeecf00a9
FelixYuanSy <ysy12141314@gmail.com>
2026-02-13 20:23:24 +0800

    test pipe chain

---
+++ test1.py
@@ -0,0 +1,32 @@
+from itertools import chain
+from typing import Union
+
+from google import genai
+from google.genai import types
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+client = genai.Client()
+
+# response = client.models.generate_content(
+#     model="gemini-3-flash-preview",
+#     config=types.GenerateContentConfig(
+#         system_instruction = "you are my study and life leader"
+#     ),
+#     contents="could you tell me who you are?"
+#
+# )
+#
+# # print(response)
+# print("-----------------------------")
+# output_text = response.text
+# # print(output_text)
+# chain = response | output_text
+# print(chain)
+
+llm = ChatGoogleGenerativeAI(model ="gemini-3-flash-preview" )
+prompt = ChatPromptTemplate.from_template("You are my study and life leader,please answer {topic} question")
+chain = prompt | llm | StrOutputParser()
+response = chain.invoke({"topic":"advantages of LangChain "})
+print(response)
